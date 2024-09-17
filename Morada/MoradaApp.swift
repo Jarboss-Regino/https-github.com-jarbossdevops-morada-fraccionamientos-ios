e819db57255ca5377850f0eb54995c4a2ad89f26
richardgonzo1 <1995rgm@gmail.com>
2024-09-17 09:55:12 -0600

    Initial Commit

---
+++ Morada/MoradaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoradaApp.swift
+//  Morada
+//
+//  Created by MacBook Air on 17/09/24.
+//
+
+import SwiftUI
+
+@main
+struct MoradaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
