3496b511df226b9f19020de7e1c226bf0d8b770d
Regino Martinez <rmartinez@jarbossdev.com>
2025-02-04 10:50:44 -0600

    Fix show registers binnacle

---
+++ Morada/Helpers/Helpes.swift
@@ -0,0 +1,8 @@
+//
+//  Helpes.swift
+//  Morada
+//
+//  Created by MacBook Air on 04/02/25.
+//
+
+import Foundation
---
+++ Morada/Helpers/Utils.swift
@@ -0,0 +1,8 @@
+//
+//  Utils.swift
+//  Morada
+//
+//  Created by MacBook Air on 04/02/25.
+//
+
+import Foundation
