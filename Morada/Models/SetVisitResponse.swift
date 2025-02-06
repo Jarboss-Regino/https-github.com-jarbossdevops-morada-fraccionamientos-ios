5762fc821b19401a38055e057c31602dbeb82554
Regino Martinez <rmartinez@jarbossdev.com>
2025-02-06 15:34:51 -0600

    Fix register visit without code qr

---
+++ Morada/Models/SetVisitResponse.swift
@@ -0,0 +1,8 @@
+//
+//  SetVisitResponse.swift
+//  Morada
+//
+//  Created by MacBook Air on 05/02/25.
+//
+
+import Foundation
---
+++ Morada/Models/requests/SetVisitRequest.swift
@@ -0,0 +1,8 @@
+//
+//  SetVisitRequest.swift
+//  Morada
+//
+//  Created by MacBook Air on 05/02/25.
+//
+
+import Foundation
