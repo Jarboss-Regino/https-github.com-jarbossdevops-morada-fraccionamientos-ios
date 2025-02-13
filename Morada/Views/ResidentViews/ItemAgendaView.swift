65eb70844fd3f662a8b8967fd3198ba05572741e
Regino Martinez <rmartinez@jarbossdev.com>
2025-02-13 15:29:01 -0600

    Add UI show agenda registers and share qr

---
+++ Morada/Views/ResidentViews/ItemAgendaView.swift
@@ -0,0 +1,18 @@
+//
+//  ItemAgendaView.swift
+//  Morada
+//
+//  Created by MacBook Air on 13/02/25.
+//
+
+import SwiftUI
+
+struct ItemAgendaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemAgendaView()
+}
